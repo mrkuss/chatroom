95c04a7e68cba10c4fdcf82aa8730c4dcb6bc57e
mrkuss <ceplismarkuss906@gmail.com>
2026-02-20 18:29:29 +0000

    server cookie thing

--- server.js
+++ server.js
@@ -100,7 +100,7 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    secure: process.env.NODE_ENV === 'production',
+    secure: true,
     httpOnly: true,
     sameSite: 'lax',
     maxAge: 7 * 24 * 60 * 60 * 1000,
